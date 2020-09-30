f3b3bcc28a253084c0eacdc1e31bca9599e41a83
tohtamysh <tohtamysh@gmail.com>
2020-09-30 12:26:18 +0300

    first

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM node:lts-alpine
+
+# Installs latest Chromium (77) package.
+RUN apk add --no-cache \
+      chromium \
+      nss \
+      freetype \
+      freetype-dev \
+      harfbuzz \
+      ca-certificates \
+      ttf-freefont
+
+# Tell Puppeteer to skip installing Chrome. We'll be using the installed package.
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
+    PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
+
+# Add user so we don't need --no-sandbox.
+RUN addgroup -S pptruser && adduser -S -g pptruser pptruser \
+    && mkdir -p /home/pptruser/Downloads /app \
+    && chown -R pptruser:pptruser /home/pptruser \
+    && chown -R pptruser:pptruser /app
+
+# Run everything after as non-privileged user.
+USER pptruser
+
+COPY ./package.json /app/package.json
+
+COPY ./package-lock.json /app/package-lock.json
+
+COPY ./server.js /app/server.js
+
+WORKDIR /app
+
+RUN npm install
+
+EXPOSE 8000
+
+ENTRYPOINT ["node", "server.js"]
---
+++ server.js
@@ -0,0 +1,57 @@
+const puppeteer = require('puppeteer');
+
+const express = require('express')
+
+const bodyParser = require('body-parser')
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const server = express()
+
+function getUrl(url, props, res) {
+  (async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--disable-dev-shm-usage', '--no-sandbox', '--disable-setuid-sandbox', '--allow-file-access-from-files', '--enable-local-file-accesses', '--disable-web-security']
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto(url, { waitUntil: 'networkidle0' })
+
+    const pdfBuffer = await page.pdf({
+      format: 'A4',
+      landscape: props.landscape
+    });
+
+    res.writeHead(200, {
+      'Content-Type': 'application/pdf',
+      'Content-Length': pdfBuffer.length
+    });
+    res.end(pdfBuffer);
+
+    await browser.close();
+  })()
+}
+
+server.post('/', urlencodedParser, function (req, res) {
+  const url = req.body.url ? req.body.url : null
+
+  const html = req.body.html ? req.body.html : null
+
+  const landscape = new Boolean(req.body.landscape)
+
+  const props = {
+    landscape: landscape,
+  }
+
+  if (!url && !html) return res.sendStatus(400)
+
+  if (url) {
+    getUrl(url, props, res)
+  }
+});
+
+server.listen(8000, () => {
+  console.log('Server started!')
+})
