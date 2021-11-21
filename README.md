061a13e75882a9ae6ce6c7e6859fccdb3da6ac42
tohtamysh <oleg@idnet.pro>
2021-11-21 13:39:47 +0300

    release v1

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# pdf-server
+# Create pdf from url or html
 
-Create pdf from html or url
+Docker image expose port: 8000
+
+Request method: POST
+
+Query params:
+
+1. url - url to pdf
+2. html - html to pdf
+3. landscape - 1 (landscape orientation& Default: 0)
--- server.go
+++ server.go
@@ -14,9 +14,7 @@ type PDFParams struct {
 }
 
 func main() {
-	http.HandleFunc("/url", urlToPDF)
-
-	http.HandleFunc("/html", htmlToPDF)
+	http.HandleFunc("/", handle)
 
 	fmt.Printf("Starting server...\n")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
@@ -24,32 +22,14 @@ func main() {
 	}
 }
 
-func urlToPDF(w http.ResponseWriter, r *http.Request) {
-	url := r.FormValue("url")
-
-	pdfParams := PDFParams{OrientationLandscape: false}
-	if r.FormValue("landscape") == "1" {
-		pdfParams.OrientationLandscape = true
-	}
-
-	if url != "" {
-		ctx, cancel := chromedp.NewContext(context.Background())
-		defer cancel()
-
-		// capture pdf
-		var buf []byte
-		if err := chromedp.Run(ctx, getURL(url, pdfParams, &buf)); err != nil {
-			log.Fatal(err)
-		}
-
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/pdf")
-		w.Write(buf)
+func handle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "Sorry, only POST method are support.")
 		return
 	}
-}
 
-func htmlToPDF(w http.ResponseWriter, r *http.Request) {
+	url := r.FormValue("url")
 	html := r.FormValue("html")
 
 	pdfParams := PDFParams{OrientationLandscape: false}
@@ -57,21 +37,43 @@ func htmlToPDF(w http.ResponseWriter, r *http.Request) {
 		pdfParams.OrientationLandscape = true
 	}
 
-	if html != "" {
-		ctx, cancel := chromedp.NewContext(context.Background())
-		defer cancel()
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
 
-		// capture pdf
-		var buf []byte
-		if err := chromedp.Run(ctx, getHTML(html, pdfParams, &buf)); err != nil {
-			log.Fatal(err)
-		}
+	var buf []byte
 
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/pdf")
-		w.Write(buf)
+	if url == "" && html == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Params error")
 		return
+	} else if url != "" {
+		buf = urlToPDF(ctx, url, pdfParams)
+	} else if html != "" {
+		buf = htmlToPDF(ctx, html, pdfParams)
 	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/pdf")
+	w.Write(buf)
+	return
+}
+
+func urlToPDF(ctx context.Context, url string, pdfParams PDFParams) []byte {
+	var buf []byte
+
+	if err := chromedp.Run(ctx, getURL(url, pdfParams, &buf)); err != nil {
+		log.Fatal(err)
+	}
+	return buf
+}
+
+func htmlToPDF(ctx context.Context, html string, pdfParams PDFParams) []byte {
+	var buf []byte
+	if err := chromedp.Run(ctx, getHTML(html, pdfParams, &buf)); err != nil {
+		log.Fatal(err)
+	}
+
+	return buf
 }
 
 func getURL(url string, params PDFParams, res *[]byte) chromedp.Tasks {
